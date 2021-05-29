fced31ef6328f5f314c5f545255fa6356ee1d082
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-05-29 14:40:45 +0530

    JavaScript: Array Destructuring

--- js/script.js
+++ js/script.js
@@ -1,37 +1,6 @@
-//ES7: Classes
-/*
-class human {
-  constructor() {
-    this.age = 30;
-  }
-  printAge() {
-    console.log(this.age);
-  }
-}
-class person extends human {
-  constructor() {
-    super();
-    this.name = "VedaGna";
-    this.age = 50;
-  }
-  printName() {
-    console.log(this.name);
-  }
-}
-const persons = new person();
-persons.printName();
-persons.printAge();
-*/
-class human {
-  age = 30;
-  printAge = () => console.log(this.age);
-}
-class person extends human {
-  name = "VedaGna";
-  age = 50;
-  printName = () => console.log(this.name);
-}
+// Array Destructuring
+[a,b] = [1,2];
+console.log(a);
+console.log(b);
 
-const persons = new person();
-persons.printName();
-persons.printAge();
+
