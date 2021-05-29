796ebc3c5fa44dac27058acc1880249b7d3e4afa
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-05-29 23:45:06 +0530

    JavaScript: Arrays

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
-// ES6: Rest Operator
-const nums = (a,b,...num) => 
-console.log(a,b,num);
-nums(1,2,3,4,5,6)
+// JavaScript: Arrays
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// array.push();
+array.push(0);
+
+// array.pop();
+array.pop();
+
+// array.shift();
+array.shift();
+
+// array.splice();
+// let index = array.indexOf(5);
+let index = 4;
+array.splice(index, 1);
+console.log(array);
