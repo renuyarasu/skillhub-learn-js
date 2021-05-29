3947dd55c833274a98654d7e0904fc5a5e431b0b
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-05-29 14:45:01 +0530

    JavaScript: Spread Operator

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
-// Array Destructuring
-[a,b] = [1,2];
-console.log(a);
-console.log(b);
+// JavaScript: Spread Operator
 
-
+//Ex:1
+[a,b, ...rest] = [1,2,3,4,5,6];
+console.log(a)
+console.log(b)
+console.log(rest)
+;
+// Ex:2
+const sum = (a,b,c) => (a+b+c);
+const numbers = [1,2,3];
+result = sum(...numbers);
+console.log(result);
