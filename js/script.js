2be2058fce54d2348466929ad6004a3c727495cb
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-05-29 23:27:04 +0530

    ES6: Rest Operator

--- js/script.js
+++ js/script.js
@@ -1,13 +1,4 @@
-// JavaScript: Destructuring Assignment
-
-let num = [10, 20];
-let [a, b] = num;
-console.log(num);
-// (2) [10, 20]
-
-let person = {
-  name: "VedaGna",
-  age: 10,
-};
-const { name, age } = person;
-console.log(name, age);
+// ES6: Rest Operator
+const nums = (a,b,...num) => 
+console.log(a,b,num);
+nums(1,2,3,4,5,6)
