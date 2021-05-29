9adf7c62e6b1438c85ec92fa8e156a17b7ed5a3d
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-05-29 14:52:25 +0530

    JavaScript: Rest Parameters

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-// JavaScript: Object Property Value Shorthand
-
-let x = 10, y = 20;
-obj = {x,y};
-console.log(obj); //Object {x: 10, y: 20}
+// JavaScript: Rest Parameters
+const foo = (x,y,...rest) => rest;
+console.log(foo(1,2,3,4,5,6,7,8,9,0)); //[3, 4, 5, 6, 7, 8, 9, 0]
