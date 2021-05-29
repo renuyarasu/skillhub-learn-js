27088b6a01bf36733d16c33e5d98af51c3c13d94
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-05-29 14:47:33 +0530

    JavaScript: String Interpolation (Template Literals)

--- js/script.js
+++ js/script.js
@@ -1,13 +1,4 @@
-// JavaScript: Spread Operator
-
-//Ex:1
-[a,b, ...rest] = [1,2,3,4,5,6];
-console.log(a)
-console.log(b)
-console.log(rest)
-;
-// Ex:2
-const sum = (a,b,c) => (a+b+c);
-const numbers = [1,2,3];
-result = sum(...numbers);
-console.log(result);
+// JavaScript: String Interpolation (Template Literals)
+let name = 'VedGna';
+let message = `Hello ${name}`;
+console.log(message);
