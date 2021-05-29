9d0642cf9abe0d45a04f7bf8dfbe2254e1bf51d9
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-05-29 14:49:57 +0530

    JavaScript: Object Property Value Shorthand

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-// JavaScript: String Interpolation (Template Literals)
-let name = 'VedGna';
-let message = `Hello ${name}`;
-console.log(message);
+// JavaScript: Object Property Value Shorthand
+
+let x = 10, y = 20;
+obj = {x,y};
+console.log(obj); //Object {x: 10, y: 20}
