08f406163c666e482a114a539a102bd6addbafa3
renukaprasad <renukaprasad.yarasu@innroad.com>
2021-07-20 15:33:51 +0530

    Destructuring

--- js/script.js
+++ js/script.js
@@ -1,12 +1,8 @@
-console.clear();
-class test {
-    constructor(num1, num2) {
-        this.num1 = num1;
-        this.num2 = num2;
-    }
-    add() {
-        return this.num1 + this.num2;
-    }
+const person = {
+    name: 'VedaGna',
+    sName: 'Yarasu',
+    age: 5,
 }
-let sum = new test(10, 20);
-console.log(sum.add());
+const { name, sName, age } = person;
+
+console.log(name);
