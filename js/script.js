1e3716d2a4ff0898ce290f6fe636ee889e7466a7
renukaprasad <renukaprasad.yarasu@innroad.com>
2021-07-20 16:53:00 +0530

    filter

--- js/script.js
+++ js/script.js
@@ -1,3 +1,3 @@
-let num1 = [1, 2, 3, 4, 5];
-let num2 = [...num1, 6, 7, 8, 9, 0];
-console.log(num2);
+const elements = [1, 2, 3, 4, 5, 6, 9, 8, 7];
+const filtered = elements.filter((element) => element > 5);
+console.log(filtered);
