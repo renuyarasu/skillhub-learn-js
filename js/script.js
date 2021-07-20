b30dadd1b968bc1cfce3f7e7bd05937c577de805
renukaprasad <renukaprasad.yarasu@innroad.com>
2021-07-20 15:47:06 +0530

    Spread

--- js/script.js
+++ js/script.js
@@ -1,8 +1,3 @@
-const person = {
-    name: 'VedaGna',
-    sName: 'Yarasu',
-    age: 5,
-}
-const { name, sName, age } = person;
-
-console.log(name);
+let num1 = [1, 2, 3, 4, 5];
+let num2 = [...num1, 6, 7, 8, 9, 0];
+console.log(num2);
