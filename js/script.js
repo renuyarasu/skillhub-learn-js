60212d15728c6f663979f646c0a03a1ef80bce16
renukaprasad <renukaprasad.yarasu@innroad.com>
2021-07-21 15:55:12 +0530

    Array: double values

--- js/script.js
+++ js/script.js
@@ -1,14 +1,7 @@
-// ES6: Destructuring 
-console.clear();
+const myarr = [1, 2, 3, 4, 5];
+const newarr = [];
 
-const name = 'Veda Ganpika Yarasu';
-const nameArr = name.split(' ');
-
-/* let fname = nameArr[0];
-let mname = nameArr[1];
-let lname = nameArr[2]; */
-
-let [fname, , lname] = nameArr;
-console.log(fname);
-// console.log(mname);
-console.log(lname);
+for (let i = 0; i < myarr.length; i++) {
+    newarr.push(myarr[i] * 10);
+}
+console.log(newarr);
--- js/tempCodeRunnerFile.js
+++ js/tempCodeRunnerFile.js
@@ -1,3 +1,1 @@
-let fname = nameArr[0];
-let mname = nameArr[1];
-let lname = nameArr[2];
+mname
