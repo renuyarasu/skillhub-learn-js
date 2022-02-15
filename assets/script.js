1606858aeefe90e4819378de1cca2ab74060ae73
Renuka Prasad <renu.yarasu@gmail.com>
2022-02-15 21:52:50 +0530

    ES6 Features

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+// Skillhub - Learn JavaScript/ES5
+console.clear();
+
+/* Array features:
+------------------
+isArray()
+map()
+filter()
+forEach()
+every()
+some()
+indexOf()
+lastIndexOf() */
+
+/* JSON features:
+-----------------
+stringify() */
+
+/* Data features:
+-----------------
+valueOf() */
+
