9ba9911f3807a0e3412609dc74a8550d9bc866d0
Renuka Prasad <renu.yarasu@gmail.com>
2022-02-15 22:03:37 +0530

    ES6 - Array features: isArray()

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,7 @@ console.clear();
 // The Array.isArray() method determines whether the passed value is an Array.
 
 let myArray = [1, 2, 3, 4, 5];
+
 let result = Array.isArray(myArray);
 console.log(result); //true
 
