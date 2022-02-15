4aeea3ad163b4d5a712ba7627587775f82b11f67
Renuka Prasad <renu.yarasu@gmail.com>
2022-02-15 21:32:49 +0530

    Sum of all numbers between 1 and 100

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+// Skillhub - Learn JavaScript
+console.clear();
+
+// Sum of all numbers between 1 and 100
+let result = 0;
+for (let i = 1; i <= 100; i++) {
+    result = result + i;
+}
+console.log(result); // 5050
