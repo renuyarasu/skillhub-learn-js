290555b0621049238a8841fe1724cf9fafa9bb9b
Renuka Prasad <renu.yarasu@gmail.com>
2022-02-15 22:25:06 +0530

    ES6 - Array features: filter() - Find Odd Numbers

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,18 @@
 // Skillhub - Learn JavaScript/ES5
 console.clear();
 
-// Array features: map()
+// Array features: filter()
 
-// The Map object holds key-value pairs and remembers the original insertion order of the keys. Any value (both objects and primitive values) may be used as either a key or a value.
+// The filter() method creates a new array with all elements that pass the test implemented by the provided function.
 
 
 
 let myArray = [1, 2, 3, 4, 5];
 
-let binary = (x) => x.toString(2);
-let result = myArray.map(binary)
+function oddValues(x) {
+    return x % 2;
+}
 
-console.log(result);['1', '10', '11', '100', '101']
+let result = myArray.filter(oddValues);
+console.log(result); //[ 1, 3, 5 ]
+
