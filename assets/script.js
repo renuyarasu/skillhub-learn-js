12e1549e0699fc0ce2a6487dc2e12c478d75f93f
Renuka Prasad <renu.yarasu@gmail.com>
2022-02-15 22:17:02 +0530

    ES6 - Array features: map()

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,17 @@
 // Skillhub - Learn JavaScript/ES5
 console.clear();
 
-// Array features: isArray()
+// Array features: map()
 
-// The Array.isArray() method determines whether the passed value is an Array.
+// The Map object holds key-value pairs and remembers the original insertion order of the keys. Any value (both objects and primitive values) may be used as either a key or a value.
+
+
 
 let myArray = [1, 2, 3, 4, 5];
 
-let result = Array.isArray(myArray);
-console.log(result); //true
 
+let result = myArray.map(function double(x) {
+    return x * 2;
+})
 
+console.log(result); // [ 2, 4, 6, 8, 10 ]
