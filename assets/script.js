a4350f12900d7279fdcf9026b3437382b6909de7
Renuka Prasad <renu.yarasu@gmail.com>
2022-02-17 16:36:50 +0530

    valueOf()

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,12 @@
 // Skillhub - Learn JavaScript/ES6
 console.clear();
 
-// The JSON.stringify() method converts a JavaScript object or value to a JSON string, optionally replacing values if a replacer function is specified or optionally including only the specified properties if a replacer array is specified.
+// Object.prototype.valueOf()
+// The valueOf() method returns the primitive value of the specified object.
 
-let data = {
-    "userId": 1,
-    "id": 1,
-    "title": "quidem molestiae enim"
-};
 
-let result = JSON.stringify(data)
+let date = new Date();
 
-console.log(result); // {"userId":1,"id":1,"title":"quidem molestiae enim"}
+let result = date.valueOf();
+
+console.log(result); 
