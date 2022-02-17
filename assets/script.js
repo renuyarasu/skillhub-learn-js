2452389959c2ae6428a1880ab5d801b5c5710728
Renuka Prasad <renu.yarasu@gmail.com>
2022-02-17 21:21:26 +0530

    ES8/ES2017 - padStart()

--- assets/script.js
+++ assets/script.js
@@ -9,4 +9,11 @@ Object.values()
 Sync/Wait
 */
 
+// String.prototype.padStart()
+// The padStart() method pads the current string with another string (multiple times, if needed) until the resulting string reaches the given length. The padding is applied from the start of the current string.
+
+const myString = 'VedaGna';
+console.log(result);
+const result = myString.padStart(10, '*');
+
 
