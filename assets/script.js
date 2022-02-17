9669ed1ad6a54a85eb74e1d96208b3e18976f538
Renuka Prasad <renu.yarasu@gmail.com>
2022-02-17 17:57:41 +0530

    ES6/ES2015 - let

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,18 @@
 // Skillhub - ES6-ES2015
 console.clear();
 // ES6-ES2015
-/* let & const
-Arrow functions
-Classes
-For..of
-Default parameters
-Rest parameters
-Spread parameters
-Destructuring */
+/* let & const / Arrow functions / Classes / For..of / Default parameters / Rest parameters / Spread parameters / Destructuring */
+// let & const
+
+
+// {
+//     var num = 10;
+// }
+
+{
+    let num = 10;
+    console.log(num); // 10
+}
+
+
 
