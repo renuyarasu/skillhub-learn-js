6b597f3cb252d2f99aac0d7e9aacb2e3980ac630
Renuka Prasad <renu.yarasu@gmail.com>
2022-02-17 16:45:41 +0530

    ES6-ES2015

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+// Skillhub - ES6-ES2015
+console.clear();
+// ES6-ES2015
+/* let & const
+Arrow functions
+Classes
+For..of
+Default parameters
+Rest parameters
+Spread parameters
+Destructuring */
+
