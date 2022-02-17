ca50fac831b34fca492ec1d8575e58f71205163d
Renuka Prasad <renu.yarasu@gmail.com>
2022-02-17 21:07:03 +0530

    ES7/ES2016 - Exponentiation operator

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+// Skillhub - ES6-ES2015
+console.clear();
+// ES7-ES2016
+/* Exponentiation operator / includes() */
+
+// Exponentiation operator (**) 
+// The exponentiation operator (**) returns the result of raising the first operand to the power of the second operand. It is equivalent to Math.pow, except it also accepts BigInts as operands.
+
+
+let result = console.log(2 ** 2); // 4
