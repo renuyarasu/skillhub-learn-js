6106091afa6b51c636b6ca207f3b6126e8b1696b
Renuka Prasad <renu.yarasu@gmail.com>
2022-02-17 16:26:38 +0530

    ES6 - Array features: indexOf()

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,13 @@
 // Skillhub - Learn JavaScript/ES5
 console.clear();
 
-// Array features: Array.prototype.every()
+// Array features: Array.prototype.indexOf()
 
-// The every() method tests whether all elements in the array pass the test implemented by the provided function. It returns a Boolean value.
+// The indexOf() method returns the first index at which a given element can be found in the array, or -1 if it is not present.
+
 
 let myArray = [1, 2, 3, 4, 5];
 
-let result = myArray.every(function (x) {
-    return x > 0;
-})
-console.log(result); // true
+let result = myArray.indexOf(5)
+
+console.log(result); // 4
