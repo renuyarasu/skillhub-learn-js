9b614bd17849e4feec47c57101a1c4953e80145e
Renuka Prasad <renu.yarasu@gmail.com>
2022-02-17 16:21:52 +0530

    ES6 - Array features: forEach()

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,22 @@
 // Skillhub - Learn JavaScript/ES5
 console.clear();
 
-// Array features: Array.prototype.reduce()
+// Array features: Array.prototype.forEach()
 
-// The reduce() method executes a user-supplied “reducer” callback function on each element of the array, in order, passing in the return value from the calculation on the preceding element. The final result of running the reducer across all elements of the array is a single value.
-
-const users = [
-
-    { firstname: 'VedaGna', lastname: 'Yarasu', age: 5 },
-    { firstname: 'Hindu', lastname: 'Sajja', age: 25 },
-    { firstname: 'Renu', lastname: 'Yarasu', age: 35 },
-    { firstname: 'Gnapika', lastname: 'Yarasu', age: 5 }
-]
-
-// Find Full Name
-// let result = users.filter(x => x.age < 30).map((x) => x.firstname);
-// Use reduce - 01
-/* let result = users.reduce((acc, cur) => {
-    if (cur.age < 30) {
-        acc = [...acc, cur.firstname];
-    }
-    return acc;
-}, []) */
+// TThe forEach() method executes a provided function once for each array element.
 
 
-let result = users.reduce((acc, cur) => {
-    if (cur.age < 30) {
-        acc.push(cur.firstname)
-    }
-    return acc;
-}, [])
 
-console.log(result); 
+let myArray = [1, 2, 3, 4, 5];
+
+
+
+let result = myArray.forEach(function (x) {
+    console.log(x);
+})
+
+/* 1
+2
+3
+4
+5 */
