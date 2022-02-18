781b5c4441f5f4c15de55c028af29fb1445a68b7
Renuka Prasad <renu.yarasu@gmail.com>
2022-02-18 16:51:21 +0530

    ES8/ ES2017 - Object.values()

--- assets/script.js
+++ assets/script.js
@@ -9,15 +9,14 @@ Object.values()
 Sync/Wait
 */
 
-// Object.entries()
-/* The Object.entries() method returns an array of a given object's own enumerable string-keyed property [key, value] pairs. This is the same as iterating with a for...in loop, except that a for...in loop enumerates properties in the prototype chain as well.
+// Object.values()
+// The Object.values() method returns an array of a given object's own enumerable property values, in the same order as that provided by a for...in loop. (The only difference is that a for...in loop enumerates properties in the prototype chain as well.)
 
-The order of the array returned by Object.entries() is the same as that provided by a for...in loop. If there is a need for different ordering, then the array should be sorted first, like Object.entries(obj).sort((a, b) => b[0].localeCompare(a[0]));. */
 
 let person = {
     name: 'VedaGna',
     age: 5,
     gender: 'Male'
 }
-let result = Object.entries(person);
-console.log(result); // [ [ 'name', 'VedaGna' ], [ 'age', 5 ], [ 'gender', 'Male' ] ]
+let result = Object.values(person);
+console.log(result); // [ 'VedaGna', 5, 'Male' ]
