dd6df182407b69a023b9019332d8c1855144157e
Renuka Prasad <renu.yarasu@gmail.com>
2022-02-18 11:41:45 +0530

    Add .gitattributes

--- js/script.js
+++
@@ -1,9 +0,0 @@
-const myarr = [1, 2, 3, 4, 5];
-const newarr = [];
-
-const doubleValues = item => item * 10;
-
-for (let i = 0; i < myarr.length; i++) {
-    newarr.push(doubleValues(myarr[i]));
-}
-console.log(newarr);
