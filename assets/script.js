7f1185551f8eaceca5daf9a4ff84aadaca341b5a
Renuka Prasad <renu.yarasu@gmail.com>
2022-02-21 14:41:21 +0530

    ES8/ ES2017 - Asynchronous

--- assets/script.js
+++ assets/script.js
@@ -17,3 +17,22 @@ console.log("0005");
 
 
 
+//Asynchronous
+
+
+console.log("0001");
+console.log("0002");
+
+setTimeout(() => {
+    console.log("0003");
+}, 4000)
+
+console.log("0004");
+console.log("0005");
+/* 
+0001
+0002
+0004
+0005
+0003 after 4s */
+
