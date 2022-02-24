d1ae48d7d57c09aa62592fdfd00389dc77c32300
Renuka Prasad <renu.yarasu@gmail.com>
2022-02-24 22:44:41 +0530

    Callback Hell - Serve ICE-CREAM

--- assets/script.js
+++ assets/script.js
@@ -5,13 +5,67 @@ console.clear();
 // Callbacks
 // When you nest a function inside another function as an argument, that's called a callback.
 
+// Let Start our ICE-CREAM Cafe
 
-function one(call_two) {
-    console.log('Step 01 completed, and please call Step 02!');
-    call_two()
-}
-function two() {
-    console.log('Step 02');
+/* 
+Order from customer 
+↓
+fetch Ingredients   
+↓
+Start Producion
+↓
+Serve
+*/
+
+
+/* TIME (seconds)
+#1: Place Order →	        2
+#2: Cut the Fruit →         2
+#3: Add Water an Ice  →     1
+#4: Start the machaine  →   1
+#5: Select container  →     2
+#6: Select toppings  →      3
+#7: Serve the ICE-CREAM  →  2
+*/
+
+let stocks = {
+    fruits: ['Strawberry', 'Apple', 'Watermelon', 'Orange', 'Pear', 'Cherry', 'Nectarine', 'Grape'],
+    liquid: ['Water', 'Ice'],
+    holder: ['Cone', 'Cup', 'Stick'],
+    toppings: ['Chocolate', 'Peanuts']
 }
 
-one(two);
+
+let order = (fruit_name, call_prodction) => {
+    setTimeout(() => {
+        console.log(`${stocks.fruits[fruit_name]} was selected!`);
+        call_prodction();
+    }, 2000)
+
+};
+
+let producion = () => {
+    setTimeout(() => {
+        console.log('Producion has started!');
+        setTimeout(() => {
+            console.log('The fruit has been chopped!');
+            setTimeout(() => {
+                console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was added`);
+                setTimeout(() => {
+                    console.log('The machine was started!');
+                    setTimeout(() => {
+                        console.log(`Ice cream was placed on ${stocks.holder[0]}`);
+                        setTimeout(() => {
+                            console.log(`${stocks.toppings[0]} was added ad toppings!`);
+                            setTimeout(() => {
+                                console.log('Serve ICE-CREAM ');
+                            }, 2000);
+                        }, 3000);
+                    }, 2000);
+                }, 1000);
+            }, 1000)
+        }, 2000)
+    }, 0)
+};
+
+order(0, producion)
