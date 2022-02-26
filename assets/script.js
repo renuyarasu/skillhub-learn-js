0dc8b97093a2bd5cc7ec664ef526313da492fa3e
Renuka Prasad <renu.yarasu@gmail.com>
2022-02-26 18:20:06 +0530

    Promises - Serve ICE-CREAM - stocks.fruits[0]

--- assets/script.js
+++ assets/script.js
@@ -28,6 +28,18 @@ Serve
 #7: Serve the ICE-CREAM  →  2
 */
 
+// Promises
+/* 
+We need to understand 4 things first:
+
+01. Relationship BTW Time and Work
+02. Promise Chaining 
+03. Error Handling
+04. The .finally handler
+
+*/
+
+
 let stocks = {
     fruits: ['Strawberry', 'Apple', 'Watermelon', 'Orange', 'Pear', 'Cherry', 'Nectarine', 'Grape'],
     liquid: ['Water', 'Ice'],
@@ -35,37 +47,21 @@ let stocks = {
     toppings: ['Chocolate', 'Peanuts']
 }
 
+let is_shop_open = true;
 
-let order = (fruit_name, call_prodction) => {
-    setTimeout(() => {
-        console.log(`${stocks.fruits[fruit_name]} was selected!`);
-        call_prodction();
-    }, 2000)
-
-};
-
-let producion = () => {
-    setTimeout(() => {
-        console.log('Producion has started!');
-        setTimeout(() => {
-            console.log('The fruit has been chopped!');
+let order = (time, work) => {
+    return new Promise((resolve, reject) => {
+        if (is_shop_open) {
             setTimeout(() => {
-                console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was added`);
-                setTimeout(() => {
-                    console.log('The machine was started!');
-                    setTimeout(() => {
-                        console.log(`Ice cream was placed on ${stocks.holder[0]}`);
-                        setTimeout(() => {
-                            console.log(`${stocks.toppings[0]} was added ad toppings!`);
-                            setTimeout(() => {
-                                console.log('Serve ICE-CREAM ');
-                            }, 2000);
-                        }, 3000);
-                    }, 2000);
-                }, 1000);
-            }, 1000)
-        }, 2000)
-    }, 0)
-};
+                resolve(work());
+            }, time);
 
-order(0, producion)
+        }
+        else {
+            reject(console.log('Our Shop is Closed!'));
+        }
+    })
+}
+
+order(2000, () => console.log(`${stocks.fruits[0]}`))
+.then
