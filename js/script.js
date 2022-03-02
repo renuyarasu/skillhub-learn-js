e009f1951afa980df8f6717d228520457281454d
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-02 19:01:49 +0530

    ES9/ES2018 - Regular Expression

--- js/script.js
+++ js/script.js
@@ -5,3 +5,8 @@ console.clear();
 • Rest/ Spread properties
   (Arguments can be transformations to a function)
 */
+
+let para = 'Spread syntax allows an iterable such as an array expression or string to be expanded in places where zero or more arguments or elements are expected, or an object expression to be expanded in places where zero or more key-value pairs (for object literals) are expected'
+
+let result = para.match(/[a-z]/g);
+console.log(result);
