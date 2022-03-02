7f46a4a8c8386cba76064056137a555ca1191c6e
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-02 19:18:44 +0530

    ES10/ES2020 - flat(2)

--- js/script.js
+++ js/script.js
@@ -12,5 +12,5 @@ console.clear();
 // Flat
 
 let numbers = [1, 2, 3, [4, 5, 6, [7, 8, 9]]];
-let results = numbers.flat(1);
-console.log(results); //[ 1, 2, 3, 4, 5, 6, [ 7, 8, 9 ] ]
+let results = numbers.flat(2);
+console.log(results); //[ 1, 2, 3, 4, 5, 6, 7, 8, 9]
