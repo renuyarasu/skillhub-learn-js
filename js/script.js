742d3262c9771bf549a64d6695d61aa80a7ac462
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-02 19:58:04 +0530

    ES11/ES2020 - Dynamic Impact

--- js/script.js
+++ js/script.js
@@ -8,12 +8,13 @@ console.clear();
 • Optional chaining 
 */
 
-// Number.MAX_SAFE_INTEGER
-// The Number.MAX_SAFE_INTEGER constant represents the maximum safe integer in JavaScript (2^53 - 1).
+// 01
+import('module.js').then((module) => {
+    //..........
+})
 
 
-
-let x = Number.MAX_SAFE_INTEGER + 1;
-let y = Number.MAX_SAFE_INTEGER + 2;
-let results = Number.MAX_SAFE_INTEGER;
-console.log(results); //  9007199254740991
+// 02
+async () => {
+    const module = await import('module.js')
+}
