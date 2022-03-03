7444f657af51ebe5a0a43112d90f44ceeae54347
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-03 17:26:59 +0530

    OOPs - Object Literal

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Skillhub - OOPs
 console.clear();
-/* 
+/*
 • OOPs Intro
 • Objects
 • Classes
@@ -13,3 +13,20 @@ console.clear();
 • Composition
 */
 
+// OOP = OOP (Object-Oriented Programming) is an approach in programming in which data is encapsulated within objects and the object itself is operated on, rather than its component parts.
+// JavaScript is heavily object-oriented. It follows a prototype-based model (as opposed to class-based).
+
+// Objects - Object, property, and method
+/* 
+    01. Object Literal
+    02. Object.create()
+    03. Functions are Objects in JavaScript 
+    04. Using Classes
+*/
+
+// Object Literal
+
+let name = { username: 'VedaGna' };
+
+console.log(name); //{ username: 'VedaGna' }
+console.log(typeof name); // object
