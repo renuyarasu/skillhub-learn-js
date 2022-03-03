cf82344b9d1cc3aa340c099fa3daf51b6c1118df
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-03 15:54:01 +0530

    ES12/ES2020 - Numeric separators

--- js/script.js
+++ js/script.js
@@ -7,9 +7,9 @@ console.clear();
 */
 
 
-// String.prototype.replaceAll()
-//The replaceAll() method returns a new string with all matches of a pattern replaced by a replacement. The pattern can be a string or a RegExp, and the replacement can be a string or a function to be called for each match.
+// Numeric separators
+// Numeric Separators give us the ability to separate thousands with an underscore (_) in numeric literals.
 
-let languege = 'Java$cript E$11';
-let result = languege.replaceAll('$', 'S');
-console.log(result);// JavaScript ES11
+
+let budget = 1_000_000_000_000;
+console.log(budget);// 1000000000000
