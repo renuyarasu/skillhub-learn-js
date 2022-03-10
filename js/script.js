92becb41a90662dc090478fb0081ec2d0bb91ce8
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-10 15:59:55 +0530

    OOPs - Classes: Inheritance - Part 01

--- js/script.js
+++ js/script.js
@@ -24,46 +24,39 @@ console.clear();
     04. Using Classes
 */
 
-/*  Abstraction
-    Abstraction in computer programming is a way to reduce complexity and allow efficient design and implementation in complex software systems. It hides the technical complexity of systems behind simpler APIs.
- */
-// Creating Abstract Class
-class Mobile {
-    constructor(name, color, initialCharging) {
+// inheritance
+// Inheritance is a mechanism where you can derive a class from another class for a hierarchy of classes that share a set of attributes and methods.
+
+// Creating Parent Class
+class Person {
+    constructor(name, designation) {
         this.name = name;
-        this.color = color;
-        this.initialCharging = initialCharging;
-        var currentCharging = 0;
-        var chargingDetails = (charge) => {
-            currentCharging = this.initialCharging + charge;
-            if (currentCharging > 100) {
-                throw Error('Mobile is fully charged!');
-            }
-            console.log('Current Charging: ' + currentCharging);
-        }
-        this.plugInCharge = (charge) => {
-            chargingDetails(charge);
-        }
+        this.designation = designation;
+    }
+    learn() {
+        console.log('Learning!');
+    }
+    eat() {
+        console.log('Eating!');
+    }
+    sleep() {
+        console.log('Sleeping!');
     }
 }
 
+// Extending Parent Class to Child Class
+
+
+// Extending Parent Class to one more Child Class
+
+
 // Creating/Installing objects by using class
-let redmi = new Mobile('Redmi Note 7', 'Black', 20);
-let apple = new Mobile('Apple', 'Black', 20);
+
+
 
 // Getting/Setting the properties and actions with created object.
-// redmi.plugInCharge(30);
-// console.log(redmi.currentCharging);
-console.log(redmi.plugInCharge(200));
-
-
-// Abstraction Interview Q/A
-
-/* 01. What exactly Abstraction means? 
-► Abstraction is the method of hiding the unwanted information. 
-02. What is the difference between Encapsulation and Abstraction? 
-► Abstraction is the method of hiding the unwanted information. Where as encapsulation is a method to hide the data in a single entity or unit along with a method to protect information from outside. */
-
+let person = new Person('Human', 'Nothing');
+console.log(person);
 
 
 
