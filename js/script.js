61b16f1c555405b45a555b6d6ffbeeaed9390c74
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-10 17:35:31 +0530

    OOPs - Classes: Advantages and Disadvantages of OOPs

--- js/script.js
+++ js/script.js
@@ -22,3 +22,28 @@ console.clear();
 5. Inheritance is a mechanism where you can derive a class from another class for a hierarchy of classes that share a set of attributes and methods. 
 6. Polymorphism means having many forms. 
 7. JavaScript supports only overriding functionality because there is no type checking functionality.
+
+
+// Advantages and Disadvantages of OOPs
+/* 
+• We can build the programs from standard working modules that communicate with one another, rather than having to start writing the code from scratch which leads to saving of development time and higher productivity,
+• OOP language allows to break the program into the bit-sized problems that can be solved easily (one object at a time).
+• The new technology promises greater programmer productivity, better quality of software and lesser maintenance cost.
+• OOP systems can be easily upgraded from small to large systems.
+• It is possible that multiple instances of objects co-exist without any interference,
+• It is very easy to partition the work in a project based on objects.
+• It is possible to map the objects in problem domain to those in the program.
+• The principle of data hiding helps the programmer to build secure programs which cannot be invaded by the code in other parts of the program.
+• By using inheritance, we can eliminate redundant code and extend the use of existing classes.
+• Message passing techniques is used for communication between objects which makes the interface descriptions with external systems much simpler.
+• The data-centered design approach enables us to capture more details of model in an implementable form.
+• While it is possible to incorporate all these features in an OOP, their importance depends upon the type of project and preference of the programmer. These technology is still developing and current products may be superseded quickly. 
+*/
+
+// Disadvantages of OOP
+/* 
+• The length of the programmes developed using OOP language is much larger than the procedural approach. Since the programme becomes larger in size, it requires more time to be executed that leads to slower execution of the programme.
+• We can not apply OOP everywhere as it is not a universal language. It is applied only when it is required.  It is not suitable for all types of problems.
+• Programmers need to have brilliant designing skill and programming skill along with proper planning because using OOP is little bit tricky.
+• OOPs take time to get used to it.  The thought process involved in object-oriented programming may not be natural for some people.
+• Everything is treated as object in OOP so before applying it we need to have excellent thinking in terms of objects.  */
